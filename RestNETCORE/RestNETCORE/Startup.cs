cf936bde2ffbac0b0eac326e65c9eb3c2ea2f60d
João Victor Gomes Cabral <jv_cabral@outlook.com>
2018-09-25 23:33:01 -0300

    Implementado mock pessoas

--- RestNETCORE/RestNETCORE/Startup.cs
+++ RestNETCORE/RestNETCORE/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestNETCORE.Services;
+using RestNETCORE.Services.Implementations;
 
 namespace RestNETCORE
 {
@@ -26,6 +28,8 @@ namespace RestNETCORE
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
